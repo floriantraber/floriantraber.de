b5cc04ad4c86801eafa017816b24196718e1fa6d
Florian Traber <florian@floriantraber.de>
2017-11-12 09:08:31 +0100

    fix hugo path

--- publish.sh
+++ publish.sh
@@ -19,7 +19,7 @@ echo "Removing existing files"
 rm -rf public/*
 
 echo "Generating site"
-../bin/hugo
+hugo
 
 echo "Updating master branch"
 cd public && git add --all && git commit -m "Publish to site (publish.sh)" && git push
