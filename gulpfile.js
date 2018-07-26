d02b77417e04c13fec58b8ba4ba08eeee150df72
Florian Traber <github@floriantraber.de>
2018-07-26 20:22:38 +0200

    Revert "wip: remove unused parts of theme"
    This reverts commit dca355e65969b1a16e3d3b3a6a6f8b976d62c98a.

--- gulpfile.js
+++ gulpfile.js
@@ -10,18 +10,39 @@ var babel = require('gulp-babel');
 let cleanCSS = require('gulp-clean-css');
 var htmlmin = require('gulp-htmlmin');
 
+// Concatenate & Minify JS
+gulp.task('scripts', function () {
+    return gulp.src('public/js/*.js')
+      //  .pipe(concat('main.js'))
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(minify())
+        .pipe(uglify())
+        .pipe(rename('main.min.js'))
+        .pipe(gulp.dest('public/js/min/'));
+});
+
+
 
 gulp.task('minify-css', () => {
     return gulp.src('public/styles.css')
         .pipe(cleanCSS({compatibility: 'ie8'}))
+        .pipe(rename('styles.min.css'))
         .pipe(gulp.dest('public/'));
 });
 
-gulp.task('minify-html', function () {
-    return gulp.src('public/**/*.html')
+gulp.task('minify-html', function() {
+    return gulp.src('public/*.html')
         .pipe(htmlmin({collapseWhitespace: true}))
         .pipe(gulp.dest('public'));
 });
 
-gulp.task('default', ['minify-css', 'minify-html']);
+gulp.task('compile', ['scripts', 'minify-css', "minify-html"], function () {
+    gulp.run("public/js/*.js", ['scripts']);
+    gulp.run("public/style.css", ['minify-css']);
+    gulp.run("public/*.html", ['minify-html']);
+});
 
+// Default Task
+gulp.task('default', ['compile']);
