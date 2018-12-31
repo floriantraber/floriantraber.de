53e5a6f601fc0c3a7b0edd66252e9e4ce194c5d9
Florian Traber <github@floriantraber.de>
2018-12-31 15:50:24 +0100

    remove all git commands as travis is handling them now.

--- publish.sh
+++ publish.sh
@@ -1,23 +1,9 @@
 #!/bin/sh
 set -e
-BRANCH=gh-pages
-if [[ $(git status -s) ]]
-then
-    echo "The working directory is dirty. Please commit any pending changes."
-    exit 1;
-fi
 
 echo "Deleting old publication"
 rm -rf public
-git worktree prune
-rm -rf .git/worktrees/public/
 mkdir public
-
-echo "Checking out $BRANCH branch into public"
-git worktree add -B $BRANCH public origin/$BRANCH
-
-echo "Removing existing files"
-rm -rf public/*
 
 echo "Generating site"
 hugo
@@ -25,8 +11,3 @@ hugo
 npm run gulp
 
 echo "floriantraber.de" >> public/CNAME
-
-# echo "Updating $BRANCH branch"
-# cd public && git add --all && git commit -m "Publish to site (publish.sh)" && git push
-
-
